f95f74aac28864209012baa0fc9d35b9689c2857
Olivier Wilkinson (reivilibre) <oliverw@matrix.org>
2021-10-15 14:39:42 +0100

    Don't bother with __version__

--- {{ cookiecutter.directory_name }}/{{ cookiecutter.package_name }}/__init__.py
+++ {{ cookiecutter.directory_name }}/{{ cookiecutter.package_name }}/__init__.py
@@ -3,9 +3,3 @@ from importlib.metadata import PackageNotFoundError, version
 from {{ cookiecutter.package_name }}.module import {{ cookiecutter.module_class_name }}
 
 __all__ = ["{{ cookiecutter.module_class_name }}"]
-
-try:
-    __version__ = version(__name__)
-except PackageNotFoundError:
-    # package is not installed
-    pass
