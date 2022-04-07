1d33e89bf45de834b4457bc935a8c1b65ce1a5bc
Brendan Abolivier <babolivier@matrix.org>
2022-04-07 13:48:35 +0100

    Improvements to the module template
    * Update linter versions to match Synapse's CI
    * Apply changes requested by Black
    * Add a header to `tests/__init__.py`

--- {{ cookiecutter.directory_name }}/tests/__init__.py
+++ {{ cookiecutter.directory_name }}/tests/__init__.py
@@ -1,7 +1,23 @@
+{% if cookiecutter.variant == "synapse_team" -%}
+# Copyright {% now 'utc', '%Y' %} The Matrix.org Foundation C.I.C.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+{% endif -%}
 from typing import Any, Dict, Optional
 
 import attr
-from mock import Mock
+from unittest.mock import Mock
 from synapse.module_api import ModuleApi
 
 from {{ cookiecutter.package_name }} import {{ cookiecutter.module_class_name }}
@@ -10,6 +26,7 @@ from {{ cookiecutter.package_name }} import {{ cookiecutter.module_class_name }}
 @attr.s(auto_attribs=True)
 class MockEvent:
     """Mocks an event. Only exposes properties the module uses."""
+
     sender: str
     type: str
     content: Dict[str, Any]
