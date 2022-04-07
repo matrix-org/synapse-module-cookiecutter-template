94ced4252be5e7728da509e72181e48018b74030
Brendan Abolivier <babolivier@matrix.org>
2022-04-07 17:50:12 +0100

    Add make_awaitable as a test util

--- {{ cookiecutter.directory_name }}/tests/__init__.py
+++ {{ cookiecutter.directory_name }}/tests/__init__.py
@@ -14,13 +14,28 @@
 # limitations under the License.
 
 {% endif -%}
-from typing import Any, Dict, Optional
+from asyncio import Future
+from typing import Any, Awaitable, Dict, Optional, TypeVar
 
 import attr
 from unittest.mock import Mock
 from synapse.module_api import ModuleApi
 
 from {{ cookiecutter.package_name }} import {{ cookiecutter.module_class_name }}
+
+TV = TypeVar("TV")
+
+
+def make_awaitable(result: TV) -> Awaitable[TV]:
+    """
+    Makes an awaitable, suitable for mocking an `async` function.
+    This uses Futures as they can be awaited multiple times so can be returned
+    to multiple callers.
+    This function has been copied directly from Synapse's tests code.
+    """
+    future = Future()  # type: ignore
+    future.set_result(result)
+    return future
 
 
 @attr.s(auto_attribs=True)
